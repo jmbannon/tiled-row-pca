e16fdad5ff43eb0e7988aba544c4bb70b9e1b22f
Jesse Bannon <jmbannon@uw.edu>
2017-05-28 11:49:33 -0700

    DGEQT2: WIP on one of the four elementary operators for tile-QR decomposition. Needs proper unit test.

--- src/test/Tests.h
+++ src/test/Tests.h
@@ -5,6 +5,7 @@ int Test_BlockMatrix_column_sums();
 int Test_DistBlockMatrix_normalize();
 int Test_BlockMatrixVector_sub();
 int Test_BlockQROperationHouse();
+int Test_BlockQROperationHouseQR();
 int Test_Matrix_copy_device_to_host();
 
 int TestAll();
