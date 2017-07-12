d93c953c942ca3c71e4ff89611bcdda043a0e42b
Jesse Bannon <jmbannon@uw.edu>
2017-07-12 16:55:52 -0700

    tile qr test wip

--- src/test/TileQR.c
+++ src/test/TileQR.c
@@ -2,6 +2,7 @@
 #include "../error.h"
 #include "../BlockMatrix.h"
 #include "DoubleCompare.h"
+#include "lapacke.h"
 #include <cuda.h>
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
@@ -14,9 +15,40 @@ int Test_TileQR(int m, int n)
     int res;
 
     BlockMatrix A;
+    Matrix expected_output;
 
     res = BlockMatrix_init_constant(&A, m, n, 3.0);
     CHECK_ZERO_ERROR_RETURN(res, "Failed to init constant block matrix");
+
+    res = Matrix_init_constant(&expected_output, m, n, 3.0);
+    CHECK_ZERO_ERROR_RETURN(res, "Failed to init constant matrix");
+
+    int min_dim = m > n ? n : m;
+    Numeric *tau = malloc(min_dim * sizeof(Numeric));
+    CHECK_MALLOC_RETURN(tau);
+
+    Numeric work_size;
+    Numeric *work;
+    int work_query = -1;
+    #if FLOAT_NUMERIC
+        LAPACK_sgeqrf(&m, &n, expected_output.data, &m, tau, &work_size, &work_query, &res);
+    #else
+        LAPACK_dgeqrf(&m, &n, expected_output.data, &m, tau, &work_size, &work_query, &res);
+    #endif
+    CHECK_ZERO_ERROR_RETURN(res, "Failed to query size of array");
+
+    int work_size_int = (int)work_size;
+    work = malloc(work_size_int * sizeof(Numeric));
+    CHECK_MALLOC_RETURN(work);
+
+    printf("starting qr\n");
+
+    #if FLOAT_NUMERIC
+        LAPACK_sgeqrf(&m, &n, expected_output.data, &m, tau, work, &work_size_int, &res);
+    #else
+        LAPACK_dgeqrf(&m, &n, expected_output.data, &m, tau, work, &work_size_int, &res);
+    #endif
+    CHECK_ZERO_ERROR_RETURN(res, "Failed to compute lapack QR decomposition");
 
     res = BlockMatrix_init_device(&A);
     CHECK_ZERO_ERROR_RETURN(res, "Failed to init block matrix on device");
@@ -30,7 +62,10 @@ int Test_TileQR(int m, int n)
     res = BlockMatrix_copy_device_to_host(&A);
     CHECK_ZERO_ERROR_RETURN(res, "Failed to copy block matrix from device to host");
 
+    printf("tile qr\n");
     BlockMatrix_print_blocks(&A);
+    printf("\nlapack\n");
+    Matrix_print(&expected_output);
 
     bool equals = false;
     return equals ? 0 : 1;
